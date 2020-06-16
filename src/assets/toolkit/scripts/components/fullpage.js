5bcafa1f9a2dc0b8c17823a564017b2b320878aa
Tara Wren <tara.r.wren@gmail.com>
2020-06-16 16:58:46 -0400

    fixed nav

--- src/assets/toolkit/scripts/components/fullpage.js
+++ src/assets/toolkit/scripts/components/fullpage.js
@@ -3,6 +3,12 @@ import fullpage from 'fullpage.js';
 import '../../../../../node_modules/fullpage.js/vendors/scrollOverflow.min';
 
 $(document).ready(() => {
+  const $navSubSection = $('[data-nav-sub-section]');
+  const $navSubSectionItem = $('[data-nav-sub-section] > li');
+  const $sectionNavParent = $('[data-section-nav-parent]');
+  const $sectionNavTitle = $('[data-section-nav-title]');
+  const $sectionTitle = $('[data-section-title]');
+
   new fullpage(`[data-fullpage]`, {
     licenseKey: '3C00B116-C1554022-9A0777F5-891F95B5',
     sectionSelector: `[data-fullpage-section]`,
@@ -45,5 +51,42 @@ $(document).ready(() => {
       'carouselPagination',
       'supportingElements'
     ],
+    afterLoad(origin, destination, direction) {
+      // hide and show sub section navs depending on active section
+      $navSubSection.each(function () {
+        var nav = $(this).attr('data-nav-sub-section');
+        var hash = window.location.hash;
+    
+        if (hash.indexOf(nav) != -1) {
+          $(this).addClass('active');
+        } else {
+          $(this).removeClass('active');
+        }
+      });
+
+      // add active class to active sub section nav link
+      $navSubSectionItem.each(function (index) {
+        var href = $(this).children('a').attr('href');
+        var hash = window.location.hash;
+    
+        if (href === hash) {
+          $(this).addClass('active');
+          $(this).siblings().removeClass('active');
+        }
+      });
+
+      // transition in active section title
+      $sectionTitle.each(function (index) {
+        if ($(this).hasClass('active')) {
+          var title = $(this).attr('data-section-title');
+          var parentNav = $(this).attr('data-section-nav-parent');
+          var parentSection = $(this).attr('data-section-parent');
+
+          $sectionNavTitle.text(title);
+          $('[data-section-nav-parent]').removeClass('active');
+          $('[data-section-nav-parent="' + parentSection + '"]').addClass('active');
+        }
+      });
+    }
   });
 });
